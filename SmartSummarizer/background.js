5bdd7800a0142931da806e35c282a8e39d34f554
Soumasish Dasgupta <72874054+Soumasish2005@users.noreply.github.com>
2025-03-13 09:47:43 +0530

    Update background.js

--- SmartSummarizer/background.js
+++ SmartSummarizer/background.js
@@ -6,7 +6,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 });
 
 async function fetchSummary(content, sendResponse) {
-  const apiKey = 'AIzaSyAzPdoHCoRtQbKYfuxHDDIS6wsBdhgy_yM'; 
+  const apiKey = 'YOUR_API_KEY';  // Insert your gemini api key here
   const apiUrl = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`;
 
   try {
